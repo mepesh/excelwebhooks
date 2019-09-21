651c3bdf059fbab2bc4cb25846952ff4c87f64ef
mepesh <dipesh.pandey42@gmail.com>
2019-09-21 14:53:12 +0545

    dsa

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const path = require("path");
-const express = require("express");
+var express = require('express');
+var app = express();
 
-const app = express();
-const port = process.env.PORT || "8000";
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello web services!');
+})
 
-app.get("/", (req, res) => {
-  res.status(200).send("WHATABYTE: Food For Devs");
-});
+// This responds a GET request for the /list_user page.
+app.get('/getajoke', function (req, res) {
+   console.log("Got a GET request for /getajoke");
+   res.send('Why are eggs not very much into jokes? Because they could crack up.');
+})
 
-app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
-});
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
